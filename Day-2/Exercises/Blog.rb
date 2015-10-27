8729633e4eee201e809322f37b3bb626b89b0d5b
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-27 16:12:18 +0100

    Edit Blog.rb with second iteration.

--- Day-2/Exercises/Blog.rb
+++ Day-2/Exercises/Blog.rb
@@ -11,30 +11,32 @@ class Blog
 
   def publish_front_page
     @posts.each do |post|
-      puts "#{post.title}\n**************\n#{post.text}\n----------------\n"
+      title = post.sponsored ? "*** #{post.title} ***" : post.title
+      puts "#{title}\n==================\n#{post.text}\n---------------------\n\n\n"
     end
   end
 end
 
 class Post
-  attr_reader :title, :date, :text
-  
-  def initialize title, date, text
+  attr_reader :title, :date, :text, :sponsored
+
+  def initialize title, date, text, sponsored = false
     @title = title
     @date = date
     @text = text
+    @sponsored = sponsored
   end
 end
 
 blog = Blog.new
 blog.add_post Post.new("Post 1", Date.new(2015, 05, 03), "This is the body of the post 1.")
-blog.add_post Post.new("Post 2", Date.new(2015, 04, 04), "This is the body of the post 2.")
+blog.add_post Post.new("Post 2", Date.new(2015, 04, 04), "This is the body of the post 2.", true)
 blog.add_post Post.new("Post 3", Date.new(2015, 10, 15), "This is the body of the post 3.")
 blog.add_post Post.new("Post 4", Date.new(2015, 12, 02), "This is the body of the post 4.")
 blog.add_post Post.new("Post 5", Date.new(2015, 03, 15), "This is the body of the post 5.")
-blog.add_post Post.new("Post 6", Date.new(2015, 01, 25), "This is the body of the post 6.")
+blog.add_post Post.new("Post 6", Date.new(2015, 01, 25), "This is the body of the post 6.", true)
 blog.add_post Post.new("Post 7", Date.new(2015, 06, 15), "This is the body of the post 7.")
 blog.add_post Post.new("Post 8", Date.new(2015, 11, 21), "This is the body of the post 8.")
-blog.add_post Post.new("Post 9", Date.new(2015, 11, 10), "This is the body of the post 9.")
+blog.add_post Post.new("Post 9", Date.new(2015, 11, 10), "This is the body of the post 9.", true)
 
 blog.publish_front_page
