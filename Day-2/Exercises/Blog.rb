d32c8d72487a7ae3742878b4432326ff1cccaeed
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-27 15:57:50 +0100

    First blog iteration.

---
+++ Day-2/Exercises/Blog.rb
@@ -0,0 +1,40 @@
+require "date"
+
+class Blog
+  def initialize
+    @posts = []
+  end
+
+  def add_post post
+    @posts << post
+  end
+
+  def publish_front_page
+    @posts.each do |post|
+      puts "#{post.title}\n**************\n#{post.text}\n----------------\n"
+    end
+  end
+end
+
+class Post
+  attr_reader :title, :date, :text
+  
+  def initialize title, date, text
+    @title = title
+    @date = date
+    @text = text
+  end
+end
+
+blog = Blog.new
+blog.add_post Post.new("Post 1", Date.new(2015, 05, 03), "This is the body of the post 1.")
+blog.add_post Post.new("Post 2", Date.new(2015, 04, 04), "This is the body of the post 2.")
+blog.add_post Post.new("Post 3", Date.new(2015, 10, 15), "This is the body of the post 3.")
+blog.add_post Post.new("Post 4", Date.new(2015, 12, 02), "This is the body of the post 4.")
+blog.add_post Post.new("Post 5", Date.new(2015, 03, 15), "This is the body of the post 5.")
+blog.add_post Post.new("Post 6", Date.new(2015, 01, 25), "This is the body of the post 6.")
+blog.add_post Post.new("Post 7", Date.new(2015, 06, 15), "This is the body of the post 7.")
+blog.add_post Post.new("Post 8", Date.new(2015, 11, 21), "This is the body of the post 8.")
+blog.add_post Post.new("Post 9", Date.new(2015, 11, 10), "This is the body of the post 9.")
+
+blog.publish_front_page
